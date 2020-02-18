e007edd4060d7847cbfb5476ebaf4b630ba10f88
Pedro Felipe <pedro.gomez.2019@upb.edu.co>
2020-02-18 09:11:26 -0500

    Little commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,6 @@
+//#####################
+//Base -2 Exercise
+//#####################
 package com.company;
 
 import java.math.BigInteger;
@@ -5,19 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static int generateNewBase(int num) {
-        //BigInteger sumatory;
-        int sumatory=0;
-        List<Integer> baseNew= new ArrayList<Integer>();
-        // Let's show the first number
-        baseNew.add(num);
-        for (int i = 1; i < num; i++) {
-
-            int result = (int) Math.pow(num, i);
-            baseNew.add(result);
-        }
-        return sumatory;
-
     }
 
     public static void main(String[] args) {

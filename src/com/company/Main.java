bca5a803ae011510da97bf0e617b10b47cbc3569
Pedro Felipe <pedro.gomez.2019@upb.edu.co>
2020-02-22 18:21:08 -0500

    Working 99.9% ; still missing when value is 0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,39 @@
 //Base -2 Exercise
 //#####################
 package com.company;
-
+//#####################
+//Import Libraries
+//#####################
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    }
+    public static void main(String[]args){
+        String casesOutput="";
+        Scanner sc= new Scanner(System.in);
+        int numCases=0;
+        numCases=sc.nextInt();
+        for (int i=0;i<numCases;i++){
+            int numRead=sc.nextInt();
+            String newNum = "";
+            while (numRead != 0)
+            {
+                int remainder = numRead % -2;
+                numRead /= -2;
 
-    public static void main(String[] args) {
+                if (remainder < 0)
+                {
+                    remainder += (-(-2));
+                    numRead += 1;
+                }
+
+                newNum = remainder + newNum;
+
+            }
+            casesOutput += "Case " + (i+1) + ": " + newNum + "\n";
+        }
+        System.out.println(casesOutput);
 
     }
 }
